2e1055c7a4c4f0e98295715db874658e29aa0b33
Lin Zhang <linroid@gmail.com>
2026-09-08 09:05:57 +0800

    refactor(torrent): reuse normalization from the metadata layer

--- library/torrent/src/iosMain/kotlin/com/linroid/ketch/torrent/TorrentIo.ios.kt
+++ library/torrent/src/iosMain/kotlin/com/linroid/ketch/torrent/TorrentIo.ios.kt
@@ -5,9 +5,6 @@ import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.usePinned
 import okio.FileSystem
 import platform.posix.arc4random_buf
-import platform.Foundation.NSString
-import platform.Foundation.create
-import platform.Foundation.precomposedStringWithCanonicalMapping
 
 internal actual val torrentFileSystem: FileSystem = FileSystem.SYSTEM
 
@@ -17,6 +14,3 @@ internal actual fun torrentRandomBytes(size: Int): ByteArray = ByteArray(size).a
 }
 
 internal actual fun createTorrentNetwork(): TorrentNetwork = PosixTorrentNetwork()
-
-internal actual fun canonicalTorrentName(value: String): String =
-  NSString.create(string = value).precomposedStringWithCanonicalMapping
--- library/torrent/src/jvmAndAndroidMain/kotlin/com/linroid/ketch/torrent/TorrentIo.jvmAndAndroid.kt
+++ library/torrent/src/jvmAndAndroidMain/kotlin/com/linroid/ketch/torrent/TorrentIo.jvmAndAndroid.kt
@@ -2,7 +2,6 @@ package com.linroid.ketch.torrent
 
 import okio.FileSystem
 import java.security.SecureRandom
-import java.text.Normalizer
 
 internal actual val torrentFileSystem: FileSystem = FileSystem.SYSTEM
 private val secureRandom = SecureRandom()
@@ -10,6 +9,3 @@ internal actual fun torrentRandomBytes(size: Int): ByteArray =
   ByteArray(size).also { secureRandom.nextBytes(it) }
 
 internal actual fun createTorrentNetwork(): TorrentNetwork = KtorTorrentNetwork()
-
-internal actual fun canonicalTorrentName(value: String): String =
-  Normalizer.normalize(value, Normalizer.Form.NFC)
